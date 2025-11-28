e49080b6c8431334db3dfc847b9032a023182a9e
kourosh <kourosh23@hotmail.com>
2025-11-28 18:43:52 +0100

    login out header

--- client/src/layout/header/header.ts
+++ client/src/layout/header/header.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AccountService } from '../../services/account-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [FormsModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  private accountService = inject(AccountService);
+  protected model: any = {};
+  protected isLoggedIn = signal(false);
+
+  onLogin() {
+    this.accountService.login(this.model).subscribe({
+      next: (result) => {
+        // console.log(result);
+        this.isLoggedIn.set(true);
+        this.model = {};
+      },
+      error: (result) => alert(result.message),
+    });
+  }
+
+  onLogout() {
+    this.isLoggedIn.set(false);
+  }
+}
